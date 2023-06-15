92b28b21a9fe95b15da0097dab195e7cffcf9ba8
Patrick <patrickfeeneytamayo@gmail.com>
2023-06-15 09:10:17 +0100

    Refactor code

--- script.js
+++ script.js
@@ -1,37 +1,30 @@
 // Global Variables used to keep track of game easily
-let playerCount = 0;
-let computerCount = 0;
+let playerWinsCount = 0;
+let computerWinsCount = 0;
 let roundCount = 0;
 
 function getComputerChoice() {
     let randomNumber = Math.floor(Math.random() * 3 + 1)
-    let computerChoice;
     switch (randomNumber) {
         case 1:
-            computerChoice = "rock";
-            break;
+            return "rock"
         case 2:
-            computerChoice = "paper";
-            break;
+            return "paper"
         case 3:
-            computerChoice = "scissors";
-            break;
-        default:
-            console.log("Something went very wrong.");
+            return "scissors"
     }
-    return computerChoice;
 }
 
-function playRound(playerSelection, computerSelection) {
+function getRoundWinner(playerSelection, computerSelection) {
     let roundResult;
     if (playerSelection === "rock") {
         if (computerSelection === "rock") {
             roundResult = "draw";
         }
-        if (computerSelection === "paper") {
+        else if (computerSelection === "paper") {
             roundResult = "computer";
         }
-        if (computerSelection === "scissors") {
+        else if (computerSelection === "scissors") {
             roundResult = "player";
         }
     }
@@ -39,10 +32,10 @@ function playRound(playerSelection, computerSelection) {
         if (computerSelection === "rock") {
             roundResult = "player";
         }
-        if (computerSelection === "paper") {
+        else if (computerSelection === "paper") {
             roundResult = "draw";            
         }
-        if (computerSelection === "scissors") {
+        else if (computerSelection === "scissors") {
             roundResult = "computer";
         }
     }
@@ -50,31 +43,27 @@ function playRound(playerSelection, computerSelection) {
         if (computerSelection === "rock") {
             roundResult = "computer";
         }
-        if (computerSelection === "paper") {
+        else if (computerSelection === "paper") {
             roundResult = "player";
         }
-        if (computerSelection === "scissors") {
+        else if (computerSelection === "scissors") {
             roundResult = "draw";
         }
     }
     return roundResult;
 }
 
-
-
-function addBounceSquaresClass() { // Rename to bounceChoiceSquares
-    function addClass() {
+function bounceChoiceContainers() {
+    function addBounceClass() {
         let squares = document.querySelectorAll(".choice-container");
         squares.forEach((square) => {
             square.classList.add("bounce");
         });
     }
 
-    function removeClass() {
+    function removeBounceClass() {
         let squares = document.querySelectorAll(".choice-container");
         squares.forEach((square) => {
-            console.log("Once");
-            // Remove the class once it finished animating!
             square.addEventListener("animationend", function handler() {
                 square.classList.remove("bounce");
                 square.removeEventListener("animationend", handler);
@@ -82,130 +71,100 @@ function addBounceSquaresClass() { // Rename to bounceChoiceSquares
         });  
     }
 
-    addClass();
-    removeClass();      
+    addBounceClass();
+    removeBounceClass();
 }
 
-function imageClicked() {
-    removeUserEventListener();
-    addBounceSquaresClass(); // Run bouncing animation on choices
+function runRound() {
+    bounceChoiceContainers();
     
-    // Player and Computer round choices
-    let choice = this.id;
+    let playerChoice = this.id;
     let computerChoice = getComputerChoice();
-    // This function will populate and update both choice squares
-    setSquaresContentWithPlayerAndComputerChoices(choice, computerChoice);
-
-    console.log(`User Choice: ${choice}, Computer Choice: ${computerChoice}`);
-    let roundResult = playRound(choice, computerChoice);
-    if (roundResult === "player") {
-        updateUserCounter(++playerCount);
-    } else if (roundResult === "computer") {
-        updateComputerCounter(++computerCount);
+    setChoiceContainersWithChoices(playerChoice, computerChoice);
+    let roundWinner = getRoundWinner(playerChoice, computerChoice);
+    console.log(`User Choice: ${playerChoice}\nComputer Choice: ${computerChoice}\nRound Winner: ${roundWinner}`);
+    if (roundWinner === "player") {
+        updatePlayerCounter(++playerWinsCount);
+    } else if (roundWinner === "computer") {
+        updateComputerCounter(++computerWinsCount);
     }
-    console.log(`Round Winner: ${roundResult}`);
-
     roundCount++;
     
     if (roundCount > 4) {
-        // The 3 images will disappear and the play again button will pop up instead of them.
-        toggleMainImages();
+        toggleRockPaperScissorsImages();
         togglePlayAgainButton();
-
-        if(playerCount === computerCount) {
-            // Game Result: Draw
-            console.log("Draw");
-            let playerResult = document.querySelector("#player-result");
-            let computerResult = document.querySelector("#computer-result");
-            playerResult.textContent = "Draw";
-            computerResult.textContent = "Draw";
-            playerResult.style.display = "block";
-            computerResult.style.display = "block";
-        } else if(playerCount < computerCount) {
-            // Game Result: Computer Wins
-            console.log("Computer Wins");
-            let playerResult = document.querySelector("#player-result");
-            let computerResult = document.querySelector("#computer-result");
-            playerResult.textContent = "Lost!";
-            computerResult.textContent = "Won!";
-            playerResult.style.display = "block";
-            computerResult.style.display = "block";
-        } else {
-            // Game Result: Player Wins
-            console.log("Player Wins");
-            let playerResult = document.querySelector("#player-result");
-            let computerResult = document.querySelector("#computer-result");
-            playerResult.textContent = "Won!";
-            computerResult.textContent = "Lost!";
-            playerResult.style.display = "block";
-            computerResult.style.display = "block";
-        }
+        showWinnerLooser();        
     } else {
-        // roundCount is not higher than 4 yet, so the game continues
-        setUserEventListener();
         updateRoundCounter(roundCount+1);
     }
 }
 
-function setSquaresContentWithPlayerAndComputerChoices(choice, computerChoice) {
-    let square1 = document.querySelector("#img-sqr-1");
-    square1.style.display = "block";    
-    if (choice === "paper") {
-        square1.src="./images/paper.svg";
-    } else if (choice === "rock") {
-        square1.src="./images/rock.svg";
-    } else {
-        square1.src="./images/scissors.svg";
+function showWinnerLooser() {
+    let playerResultSpan = document.querySelector("#player-result");
+    let computerResultSpan = document.querySelector("#computer-result");
+    playerResultSpan.style.display = "block";
+    computerResultSpan.style.display = "block";
+    if(playerWinsCount === computerWinsCount) {
+        console.log("Game Result: Draw");
+        playerResultSpan.textContent = "Draw";
+        computerResultSpan.textContent = "Draw";
+    } else if(playerWinsCount < computerWinsCount) {
+        console.log("Game Result: Computer Wins");
+        playerResultSpan.textContent = "Lost!";
+        computerResultSpan.textContent = "Won!";
+    } else if(playerWinsCount > computerWinsCount) {
+        console.log("Game Result: Player Wins");
+        playerResultSpan.textContent = "Won!";
+        computerResultSpan.textContent = "Lost!";
     }
-    let square2 = document.querySelector("#img-sqr-2");
-    square2.style.display = "block";
-    if (computerChoice === "paper") {
-        square2.src="./images/paper.svg";
-    } else if (computerChoice === "rock") {
-        square2.src="./images/rock.svg";
+}
+
+function setChoiceContainersWithChoices(playerChoice, computerChoice) {
+    let imageChoiceLeft = document.querySelector("#img-sqr-1");
+    imageChoiceLeft.style.display = "block";    
+    if (playerChoice === "paper") {
+        imageChoiceLeft.src="./images/paper.svg";
+    } else if (playerChoice === "rock") {
+        imageChoiceLeft.src="./images/rock.svg";
     } else {
-        square2.src="./images/scissors.svg";
+        imageChoiceLeft.src="./images/scissors.svg";
+    }
+    let imageChoiceRight = document.querySelector("#img-sqr-2");
+    imageChoiceRight.style.display = "block";
+    if (computerChoice === "paper") {
+        imageChoiceRight.src="./images/paper.svg";
+    } else if (computerChoice === "rock") {
+        imageChoiceRight.src="./images/rock.svg";
+    } else {
+        imageChoiceRight.src="./images/scissors.svg";
     }
 }
 
 function emptyChoiceSquares() {
-    let squares = document.querySelectorAll(".choice-images");
-    squares.forEach((square) => {
-        square.style.display = "none";
+    let images = document.querySelectorAll(".choice-images");
+    images.forEach((image) => {
+        image.style.display = "none";
     })
 }
 
-function game() { // Change this to gameSetup?
-    hidePlayersResultText(); // Hide Win/Lost titles at the beginning of the game
-    setUserEventListener(); // Listen for player's clicks
-    setButtonEventListener(); // Listen for Play Again button click
-}
-
-function hidePlayersResultText() {
+function hideWinnerLooserResultTexts() {
     let playerResult = document.querySelector("#player-result");
     playerResult.style.display = "none";
     let computerResult = document.querySelector("#computer-result");
     computerResult.style.display = "none";
 }
 
-function toggleMainImages() {
+function toggleRockPaperScissorsImages() {
     let mainImages = document.querySelectorAll(".main-images");
-    let displayType = window.getComputedStyle(mainImages[0]).getPropertyValue("display");
-    if (displayType === "inline") {
-        console.log("Setting display to none");
+    let displayTypeImage1 = window.getComputedStyle(mainImages[0]).getPropertyValue("display");
+    if (displayTypeImage1 === "inline") {
         mainImages.forEach((image) => {
             image.style.display = "none";
         });
-    } else if (displayType === "none") {
-        console.log("Setting display to inline");
+    } else {
         mainImages.forEach((image) => {
             image.style.display = "inline";
         });
-    } else {
-        console.log("Something isn't right. Setting display property of images to inline");
-        mainImages.forEach((image) => {
-            image.style.display = "inline";
-        })
     }
 }
 
@@ -213,52 +172,28 @@ function togglePlayAgainButton() {
     let playAgainButton = document.querySelector("#play-again");
     let displayType = window.getComputedStyle(playAgainButton).getPropertyValue("display");
     if (displayType === "inline-block") {
-        console.log("Setting display to none");
         playAgainButton.style.display = "none";
-    } else if (displayType === "none") {
-        console.log("Setting display to inline");
-        playAgainButton.style.display = "inline";
     } else {
-        console.log("Something isn't right. Setting display property of button to inline");
         playAgainButton.style.display = "inline";
     }
 }
 
-function restartGame() {
-    playerCount = 0;
-    computerCount = 0;
-    roundCount = 0;
-    console.log("Restarting game");
-    updateRoundCounter(roundCount+1);
-    updateUserCounter(playerCount);
-    updateComputerCounter(computerCount);
-    toggleMainImages();
-    togglePlayAgainButton();
-    emptyChoiceSquares();
-    game();
-}
-
 // Event Listeners
-function setButtonEventListener() {
+function setPlayAgainButtonEventListener() {
     let buttonElement = document.querySelector("#play-again");
-    buttonElement.addEventListener("click", restartGame);
+    buttonElement.addEventListener("click", gameSetup);
 }
-function setUserEventListener() {
+function setImagesClickEventListener() {
     const images = document.querySelectorAll(".main-images");
-    images.forEach(image => image.addEventListener("click", imageClicked));   
+    images.forEach(image => image.addEventListener("click", runRound));   
 }
-function removeUserEventListener() {
-    const images = document.querySelectorAll(".main-images");
-    images.forEach(image => image.removeEventListener("click", imageClicked));
-}
-
 
 // Counters functions
 function updateRoundCounter(roundNumber) {
     let spanElement = document.querySelector("#round-count");
     spanElement.textContent = roundNumber;
 }
-function updateUserCounter(userScore) {
+function updatePlayerCounter(userScore) {
     let spanElement = document.querySelector("#player-score");
     spanElement.textContent = userScore;
 }
@@ -267,4 +202,20 @@ function updateComputerCounter(computerScore) {
     spanElement.textContent = computerScore;
 }
 
-game();
+function gameSetup() {
+    playerWinsCount = 0;
+    computerWinsCount = 0;
+    roundCount = 0;
+    hideWinnerLooserResultTexts();
+    updateRoundCounter(roundCount+1);
+    updatePlayerCounter(playerWinsCount);
+    updateComputerCounter(computerWinsCount);
+    // Toggling works because the initial state of the images is none, so it gets set to inline.
+    togglePlayAgainButton();
+    toggleRockPaperScissorsImages();
+    emptyChoiceSquares();
+    setImagesClickEventListener();
+    setPlayAgainButtonEventListener();
+}
+
+gameSetup();
